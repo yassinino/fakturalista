29c1d4d0c9b7e04d9bfc295a7ce28cd2186c0536
Yassine AIT TOUIJAR <aittouijar.yassine@gmail.com>
2025-11-24 16:57:42 +0100

    fix it now migrations

--- database/migrations/2016_06_01_000002_create_oauth_access_tokens_table.php
+++ database/migrations/2016_06_01_000002_create_oauth_access_tokens_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('oauth_access_tokens', function (Blueprint $table) {
-            $table->string('id', 100)->primary();
+            $table->uuid('id')->primary();
             $table->unsignedBigInteger('user_id')->nullable()->index();
             $table->uuid('client_id');
             $table->string('name')->nullable();
