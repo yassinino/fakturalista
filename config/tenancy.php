e48da3cac35e59c9b39b9b924f05d1da95897cc8
Yassine AIT TOUIJAR <aittouijar.yassine@gmail.com>
2025-12-12 18:52:51 +0100

    central website

--- config/tenancy.php
+++ config/tenancy.php
@@ -17,6 +17,8 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
+        'fakturalista.com',
+        'www.fakturalista.com',
         '127.0.0.1',
         'localhost',
     ],
